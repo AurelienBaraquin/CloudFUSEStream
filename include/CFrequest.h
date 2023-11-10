404ae28bef2356e836d4d7d876102760e70d6541
aurelienBaraquin <aurelien.baraquin@epitech.eu>
2023-11-10 11:04:09 +0100

    Update comment style in CFrequest.h

--- include/CFrequest.h
+++ include/CFrequest.h
@@ -10,7 +10,7 @@ typedef struct section_s {
     char *data;
 } CFsection;
 
-// contains the request data, which is a list of sections which each section pointing to a data buffer in the data field
+/* Contains the request data, which is a list of sections which each section pointing to a data buffer in the data field */
 typedef struct CFreq_s {
     size_t size;
     CFsection *sections;
